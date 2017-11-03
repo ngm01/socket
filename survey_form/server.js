209649cced5be70ca3058294308d45b9296e5f53
ngm84 <nathaniel.g.moore@gmail.com>
2017-11-03 16:40:59 -0400

    survey form -- learning sockets

---
+++ survey_form/server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, "./static")));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    res.render("index");
+})
+
+var server = app.listen(8000, function(){
+    var line = "\n**************************\n"
+    console.log(line, "Survey Form Revisted", "\n" , "Listening on Port 8000", line);
+})
+var io = require('socket.io').listen(server);
+var results;
+io.sockets.on('connection', function(socket){
+    //server socket code goes here...
+    socket.emit('updated_message', results);
+    socket.on('posting_form', function(data){
+        // console.log("Just data:",data);
+        // console.log("data.test:",data.test);
+        console.log("Are we getting comment?", data.comment);
+        results = data;
+        results.number = Math.floor(Math.random() * 1001);
+        console.log(results);
+        
+    })
+})
