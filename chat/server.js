6fb9f3823a0eacb7093843b1fe8d5dc2a90b0235
ngm84 <nathaniel.g.moore@gmail.com>
2017-11-06 10:54:16 -0500

    commented out an unnecessary function

--- chat/server.js
+++ chat/server.js
@@ -19,26 +19,28 @@ var users = [];
 var posts = [];
 io.sockets.on('connect', function(socket){
     io.emit("users", users);
-    io.emit("posts", populatePostsUsers());
+    io.emit("posts", posts);
     socket.on("new_user", (user_info)=>{
         users.push(user_info);
         io.emit("users", users);
-        socket.emit("posts", populatePostsUsers());
+        socket.emit("posts", posts);
         //need to incorporate session into this?
     })
     socket.on("new_msg", (msg_info)=>{
         posts.push(msg_info);
-        io.emit("posts", populatePostsUsers());
+        io.emit("posts", posts);
     })
     
 })
-function populatePostsUsers(){
-    for(let i=0;i<posts.length;i++){
-        for(let k=0;k<users.length;k++){
-            if(posts[i].user_id == users[k].user_id){
-                posts[i].user_name = user[k].user;
-            }
-        }
-    }
-    return posts;
-}
+
+// unnecesary
+// function populatePostsUsers(){
+//     for(let i=0;i<posts.length;i++){
+//         for(let k=0;k<users.length;k++){
+//             if(posts[i].user_id == users[k].user_id){
+//                 posts[i].user_name = user[k].user;
+//             }
+//         }
+//     }
+//     return posts;
+// }
